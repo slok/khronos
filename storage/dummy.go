57e5f365ad560b3cda711521c2a1b653990964a9
Xabier Larrakoetxea <slok69@gmail.com>
2016-03-01 19:17:46 +0100

    Add support for result storage on dummy client
    Signed-off-by: Xabier Larrakoetxea <slok69@gmail.com>

--- storage/dummy.go
+++ storage/dummy.go
@@ -11,6 +11,8 @@ import (
 )
 
 const jobKeyFmt = "job:%d"
+const jobResultsKeyFmt = "job:%d:results"
+const resultKeyFmt = "result:%d"
 
 // Dummy implements the Storage interface everything to a local memory map
 type Dummy struct {
@@ -18,6 +20,10 @@ type Dummy struct {
 	Jobs       map[string]*job.Job
 	JobCounter int
 	jobsMutex  *sync.Mutex
+
+	Results        map[string]map[string]*job.Result
+	ResultsCounter map[string]int
+	resultsMutex   *sync.Mutex
 }
 
 // NewDummy creates a client that stores on memory
@@ -27,6 +33,10 @@ func NewDummy() *Dummy {
 		Jobs:       map[string]*job.Job{},
 		JobCounter: 0,
 		jobsMutex:  &sync.Mutex{},
+
+		Results:        map[string]map[string]*job.Result{},
+		ResultsCounter: map[string]int{},
+		resultsMutex:   &sync.Mutex{},
 	}
 }
 
@@ -108,10 +118,92 @@ func (c *Dummy) DeleteJob(j *job.Job) error {
 
 	key := fmt.Sprintf(jobKeyFmt, j.ID)
 
-	if _, ok := c.Jobs[key]; !ok {
-		return errors.New("Not existent job")
+	delete(c.Jobs, key)
+	// Don't return error if the job doens't exists
+
+	return nil
+}
+
+// GetResults returns results from a job on memory
+func (c *Dummy) GetResults(j *job.Job, low, high int) ([]*job.Result, error) {
+	c.resultsMutex.Lock()
+	defer c.resultsMutex.Unlock()
+
+	results, ok := c.Results[fmt.Sprintf(jobResultsKeyFmt, j.ID)]
+
+	if !ok {
+		return nil, errors.New("Wrong job")
 	}
 
-	delete(c.Jobs, key)
+	if results == nil {
+		return nil, errors.New("Error retrieving jobs")
+	}
+
+	// High on top means all
+	if high == 0 {
+		high = len(results)
+	}
+
+	// Check indexes ok
+	if low > high || low > len(results) || high > len(results) {
+		return nil, errors.New("wrong parameters")
+	}
+
+	res := []*job.Result{}
+	for i := low; i < high; i++ {
+		res = append(res, results[fmt.Sprintf(resultKeyFmt, i)])
+	}
+	return res, nil
+}
+
+// GetResult returns a result from a job on memory
+func (c *Dummy) GetResult(j *job.Job, id int) (*job.Result, error) {
+	c.resultsMutex.Lock()
+	defer c.resultsMutex.Unlock()
+
+	results, ok := c.Results[fmt.Sprintf(jobResultsKeyFmt, j.ID)]
+	if !ok {
+		return nil, errors.New("Wrong job")
+	}
+
+	res, ok := results[fmt.Sprintf(resultKeyFmt, id)]
+	if !ok {
+		return nil, errors.New("Not existent result")
+	}
+
+	return res, nil
+}
+
+// SaveResult saves a result on a job in memory
+func (c *Dummy) SaveResult(r *job.Result) error {
+	c.resultsMutex.Lock()
+	defer c.resultsMutex.Unlock()
+
+	resultsKey := fmt.Sprintf(jobResultsKeyFmt, r.Job.ID)
+	c.ResultsCounter[resultsKey]++
+	r.ID = c.ResultsCounter[resultsKey]
+	results, ok := c.Results[resultsKey]
+	if !ok {
+		results = map[string]*job.Result{}
+		c.Results[resultsKey] = results
+	}
+	results[fmt.Sprintf(jobResultsKeyFmt, r.ID)] = r
+
+	return nil
+}
+
+// DeleteResult deletes a job on ajob in memory
+func (c *Dummy) DeleteResult(r *job.Result) error {
+	c.resultsMutex.Lock()
+	defer c.resultsMutex.Unlock()
+
+	results, ok := c.Results[fmt.Sprintf(jobResultsKeyFmt, r.Job.ID)]
+	if !ok {
+		return errors.New("Wrong job")
+	}
+
+	delete(results, fmt.Sprintf(jobResultsKeyFmt, r.ID))
+
+	// Don't return error if the result doens't exists
 	return nil
 }
--- storage/storage.go
+++ storage/storage.go
@@ -26,15 +26,14 @@ type Client interface {
 	DeleteJob(j *job.Job) error
 
 	// Result actions
-	// GetResults returns an slice of results from a job; jobID parameter is the
-	// id of the job from the results will be obtained. The low parmeter will be the
+	// GetResults returns an slice of results from a job; The low parmeter will be the
 	// first result and the high will be the next one to the last result that will be
 	// returned; this acts like an slice operator. 0 on high parameter means all.
 	// this would be translated as results[low:] and 0 on low would be results[:high]
-	GetResults(jobID, low, high int) ([]*job.Result, error)
+	GetResults(j *job.Job, low, high int) ([]*job.Result, error)
 
-	// GetResult returns a result based on the id
-	GetResult(id int) (*job.Result, error)
+	// GetResult returns a result based on the result id of a job
+	GetResult(j *job.Job, id int) (*job.Result, error)
 
 	// SaveResult stores the result.  Results cannot be updated
 	SaveResult(r *job.Result) error
